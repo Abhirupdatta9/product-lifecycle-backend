bc74fbc8bef4c2002f57f431f32dba252e3ef3fa
banerjee-suchismita <suchismita.b07@gmail.com>
2020-04-05 00:29:39 +0530

    User Login update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserLoginRequest;
+use App\Http\Resources\UserResource;
 
 class AuthController extends Controller
 {
-    //
+    public function login(UserLoginRequest $request){
+        if(!$token = auth()->attempt($request->only(['name','email','password']))) {
+            return response()->json([
+                'errors' => [
+                    'email' => ['Record not found']
+                ]
+            ]);
+        };
+
+        return (new UserResource($request->user()))->additional([
+            'meta' => [
+                'token' => $token,
+            ],
+        ]);
+    }
 }
